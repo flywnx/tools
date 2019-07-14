f5510f4d956e13eea3e7f6c0123947e2feb9102e
jianglibo <781630130@qq.com>
2019-07-14 21:33:54 +0800

    批量重命名工具完成

--- batch_rename/README.md
+++ batch_rename/README.md
@@ -1,7 +1,19 @@
 1.这是用electron 写的 一个桌面应用，用于批量修改文件夹内的文件名字
 
+2.发布记录：
+    npm install electron-packager -g 安装打包组件
+
+    package.json 添加： "package": "electron-packager 打包路径 --all --out 输出路径 --overwrite --icon=icon路径"
+
+    npm install -g asar
+
+    asar pack 文件夹路径 项目名.asar  
+
+    发布成exe的问题。需要安装electron-prebuilt   npm install -g electron-prebuilt
+
+    然后运行 npm run-script package 即可
+
 后续构思：
-   
     写一个文件选择窗口，用于选择文件夹
     实现拖动文件读取路径
 
--- batch_rename/index.js
+++ batch_rename/index.js
@@ -1,7 +1,27 @@
 const fs = require('fs')
-function onGetFolder() {
-    console.log(__dirname)
-    fs.readdir('./', function (err, file) {
+
+function startChange() {
+    let input = document.getElementById('input').value,
+        output = document.getElementById('output').value,
+        num = 1,
+        filetype = document.getElementById('filetype').value,
+        prefix = document.getElementById('prefix').value,
+        suffix = document.getElementById('suffix').value
+    
+    if(!output) output = input
+    fs.readdir(input, function (err, file) {
         console.log(err, file)
+        file.forEach(element => {
+            let arr = element.split('.')
+            if (arr.length <= 1) {
+                return
+            }
+            let oldPath = input + "\\" + element,
+                newPath = output + "\\" + prefix + num + suffix + '.' + filetype
+            fs.rename(oldPath, newPath, () => {
+                console.log(newPath)
+            })
+            num += 1
+        })
     })
 }
