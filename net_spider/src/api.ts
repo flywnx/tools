53c67e42c48d24745ec661d8ef539062506d22ac
jianglibo <781630130@qq.com>
2019-07-18 20:49:15 +0800

    ts配置

--- net_spider/src/api.ts
+++ net_spider/src/api.ts
@@ -1,15 +1,18 @@
 import request from 'request'
 import cheerio from 'cheerio'
+import fs from 'fs';
+import config from './config';
 
 
 export default class api {
     public static setReq(path: string): void {
-        request(path, (error, response, body) => {
+        request(config.PATH
+        , (error, response, body) => {
+            console.log(error)
+            console.log(response.statusCode)
             if (!error && response.statusCode == 200) {
-                // console.log(body)
-                // console.log(typeof body);
                 api.saveInfo(body, path)
-                // return body
+
             }
         })
     }
@@ -17,10 +20,15 @@ export default class api {
         let doc = cheerio.load(data)
         // console.log(doc)
 
-        let node_a = doc("a").html()
-        let node_ul = doc("ul").html()
-        console.log(node_a);
-        console.log(node_ul);
+        // let node_a = doc("a").html()
+        // let node_ul = doc("ul").html()
+        // console.log(node_a);
+        // console.log(node_ul);
+        console.log('获取成功');
+        fs.writeFile('./index.html', data, () => {
+            console.log('写入成功');
+
+        })
 
         // if (config.pathArr.indexOf(path) < 0) {
         //     config.pathArr.push(path)
--- net_spider/src/config.ts
+++ net_spider/src/config.ts
@@ -1,7 +1,7 @@
 
 export default class config {
     //起始路径
-    public static PATH: string = 'https://movie.douban.com/'
+    public static PATH: string = 'http://www.tmsf.com/newhouse/property_searchall.htm?searchkeyword=&keyword=&sid=&districtid=&areaid=&dealprice=&propertystate=&propertytype=&ordertype=&priceorder=&openorder=&view720data=&page=2&bbs=&avanumorder=&comnumorder='
     //收集过的路径
     public static pathArr: Array<string> = []
 }
